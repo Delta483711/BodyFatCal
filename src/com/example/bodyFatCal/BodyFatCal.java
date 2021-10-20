33de728d81590149f7e0f3da3100536f1bbf2391
Michael <Michael483711@outlook.com>
2021-10-20 18:17:30 +0100

    Added "!!"

--- src/com/example/bodyFatCal/BodyFatCal.java
+++ src/com/example/bodyFatCal/BodyFatCal.java
@@ -2,7 +2,8 @@ package com.example.bodyFatCal;
 
 public class BodyFatCal {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        System.out.println("hello world!!");
 
     }
 }
