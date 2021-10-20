418a7eed49589775e0d776e2dafae28146056168
Michael <Michael483711@outlook.com>
2021-10-20 17:44:59 +0100

    Added one more word

--- src/com/example/bodyFatCal/BodyFatCal.java
+++ src/com/example/bodyFatCal/BodyFatCal.java
@@ -2,7 +2,7 @@ package com.example.bodyFatCal;
 
 public class BodyFatCal {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
 
     }
 }
