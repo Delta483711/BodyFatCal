e666e23395e9dd40bfa39ac212eb3d9d452e53b9
Michael <Michael483711@outlook.com>
2021-10-20 18:44:41 +0100

    Added some more functionality.

--- src/com/example/bodyFatCal/BodyFatCal.java
+++ src/com/example/bodyFatCal/BodyFatCal.java
@@ -1,12 +1,30 @@
 package com.example.bodyFatCal;
 
+//Import
+import java.util.Scanner;
+
 public class BodyFatCal {
 
-    static void Test() {
-        System.out.println("I just got executed!");
-    }
 
     public static void main(String[] args) {
-        Test();
+
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.println("This is the First Iteration of the App " +
+                "that will Calculate Body Fat Percentage. Type Yes to continue or No to Leave");
+        String  input = Sc.nextLine();
+
+        if(input.equals("Yes")){
+            System.out.println("Enter your Height (Cm)");
+            float Height = Sc.nextFloat();
+
+            System.out.println("Enter your Weight(Kg)");
+            float Weight = Sc.nextFloat();
+        }
+        else if(input.equals("No")){
+            System.out.println("Thank you.");
+            Sc.nextLine();
+            Sc.close();
+        }
     }
 }
