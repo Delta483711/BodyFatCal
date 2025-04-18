108bbcefb50c4c10465dffec847a6d24c2089035
Delta <59081538+Delta483711@users.noreply.github.com>
2025-04-18 16:37:03 +0100

    Python iteration02 (#3)
    * Separated files
    
    * Update Python_BodyFatCal_Iteration02.py
    
    * Update file 3
    
    * Update files
    
    For iteration 01 - Removed comments
    For Iteration02 - Added all the logic

--- Python/Python_BodyFatCal_Iteration01.py
+++ Python/Python_BodyFatCal_Iteration01.py
@@ -8,7 +8,6 @@
 #Declare Variables 
 
 Age = 30;
-Age = 0;
 Height = 178;
 Weight = 80 ;
 Gender = 1  # 1 = Male, 0 = Female
@@ -23,13 +22,6 @@ BMI = Weight / (Height/100 * Height/100);
 print("Your BMI is:"); 
 print(round(BMI,1));
 
-
-# Logic for BMI 
-
-BMI = Weight / (Height/100 * Height/100);
-print("Your BMI is:"); 
-print(round(BMI,1));
-
 # Logic for Body fat %
 
 BFP = (1.20 * BMI) + (0.23 * Age) - (10.8 * Gender) - 5.4;
@@ -38,31 +30,6 @@ BFP = (1.20 * BMI) + (0.23 * Age) - (10.8 * Gender) - 5.4;
 
 print("Your Body Fat Percentage is: ");
 print(round(BFP,1)); 
-
-
-
-
-#----------------------------------------------------------------------------------------#
-# Create whole program 2nd iteration - User input 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#----------------------------------------------------------------------------------------#
-# 3rd iteration - Create Functions to store different logics and call them. 
 
 
 
---
+++ Python/Python_BodyFatCal_Iteration03.py
@@ -0,0 +1,7 @@
+
+# Python - Body Fat Calculator # 
+
+#----------------------------------------------------------------------------------------#
+
+# 3rd iteration - Create Functions to store different logics and call them. 
+
