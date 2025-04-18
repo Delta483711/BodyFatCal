4964dd5e841c0cc8c122a47c66f85561c6ddc4cc
Delta <Michael483711@outlook.com>
2025-04-18 15:56:46 +0100

    Added  comments and formatted code

--- Python/Python_BodyFatCal.py
+++ Python/Python_BodyFatCal.py
@@ -3,28 +3,30 @@
 
 #----------------------------------------------------------------------------------------#
 
-
 # Create whole program 1st iteration - hard coded
 
 #Declare Variables 
 
-#BMI = 0;
-Age = 0;
+Age = 30;
 Height = 178;
 Weight = 80 ;
 Gender = 1  # 1 = Male, 0 = Female
 
-
 # Logic for BMI 
 
 BMI = Weight / (Height/100 * Height/100);
+
+# Print the results 
+
 print("Your BMI is:"); 
 print(round(BMI,1));
-
 
 # Logic for Body fat %
 
 BFP = (1.20 * BMI) + (0.23 * Age) - (10.8 * Gender) - 5.4;
+
+
+# Print the results 
 
 print("Your Body Fat Percentage is: ");
 print(round(BFP,1)); 
