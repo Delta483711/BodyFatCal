162272552ec7436a8b5de9d1bf924f37a26b3bcc
Delta <59081538+Delta483711@users.noreply.github.com>
2025-04-18 16:00:12 +0100

    Python iteration01 (#2)
    * Added the first Iteration of BFP
    
    * Add space
    
    * Added  comments and formatted code

--- Python/Python_BodyFatCal.py
+++ Python/Python_BodyFatCal.py
@@ -3,12 +3,11 @@
 
 #----------------------------------------------------------------------------------------#
 
-
 # Create whole program 1st iteration - hard coded
 
 #Declare Variables 
 
-#BMI = 0;
+Age = 30;
 Age = 0;
 Height = 178;
 Weight = 80 ;
@@ -18,31 +17,27 @@ Gender = 1  # 1 = Male, 0 = Female
 # Logic for BMI 
 
 BMI = Weight / (Height/100 * Height/100);
+
+# Print the results 
+
 print("Your BMI is:"); 
 print(round(BMI,1));
 
+
+# Logic for BMI 
+
+BMI = Weight / (Height/100 * Height/100);
+print("Your BMI is:"); 
+print(round(BMI,1));
 
 # Logic for Body fat %
 
 BFP = (1.20 * BMI) + (0.23 * Age) - (10.8 * Gender) - 5.4;
 
+# Print the results 
+
 print("Your Body Fat Percentage is: ");
 print(round(BFP,1)); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
