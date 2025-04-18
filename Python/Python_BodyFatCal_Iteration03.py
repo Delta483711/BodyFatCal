d7bb04def3e1204ea4d16aefd51a98bc2e6ee0b9
Delta <Michael483711@outlook.com>
2025-04-18 18:38:42 +0100

    Add new file to store functions and add iteration03

--- Python/Python_BodyFatCal_Iteration03.py
+++ Python/Python_BodyFatCal_Iteration03.py
@@ -1,3 +1,5 @@
+from Python_Functions import CalBMI, CalBFP
+
 
 # Python - Body Fat Calculator # 
 
@@ -5,3 +7,28 @@
 
 # 3rd iteration - Create Functions to store different logics and call them. 
 
+Age = 30;
+Height = 178;
+Weight = 80 ;
+Gender = 1  # 1 = Male, 0 = Female
+
+
+# Call function for getting BMI
+
+BMI = CalBMI(Weight,Height)
+
+
+# Print the results 
+
+print("Your BMI is:"); 
+print(round(BMI,1));
+
+
+# Call function for getting body fat percentage 
+
+BodyFatPercent = CalBFP(Weight,Height,Age,Gender)
+
+# Print the results 
+
+print("Your Body Fat Percentage is: ");
+print(round(BodyFatPercent,1));
